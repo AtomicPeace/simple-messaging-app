b8b60ebfd5b72c1af993444ef724607098e2a9eb
Lawrence Horton <poxopox1995@gmail.com>
2019-03-09 14:06:00 -0500

    added a mother fucking clock you bastards will pay

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 
 const input = document.getElementById("message-input-box");
 const sendButton = document.getElementById("message-send-button");
+const clock = document.getElementById("clock");
 const {ipcRenderer} = require('electron');
 
 
@@ -20,3 +21,8 @@ sendButton.addEventListener('click', (e)=>{
     console.log({payload: input.value});
     ipcRenderer.send('asynchronous-message',  {payload: input.value});
 });
+
+setInterval(()=>{
+    const date = new Date();
+    clock.innerText = `${date.getHours()% 12}:${date.getMinutes()}:${date.getSeconds()} :: ${date.getMilliseconds()}`;
+}, 10)
